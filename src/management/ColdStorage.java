060dab1d02c74c038337fc1729a21224b880690f
an <an07042k4@gmail.com>
2024-05-19 08:27:59 +0700

    8:27 19/5/2024: L Khanh sua

--- src/management/ColdStorage.java
+++ src/management/ColdStorage.java
@@ -1,12 +1,12 @@
 package management;
 
-import management.views.categories.device.DeviceJFrame;
+import management.views.main.MainJFrame;
 
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class ColdStorage {
 
     public static void main(String[] args) throws MqttException{
-        new DeviceJFrame().setVisible(true);
+        new MainJFrame().setVisible(true);
     }
 }
